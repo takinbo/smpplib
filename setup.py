9d88647d78b1f119de5799efea54a078a0be853e
Tim Akinbo <takinbo@gmail.com>
2010-12-09 14:48:12 +0100

    SMPP Library for Python

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(name='smpplib',
+      version='0.1',
+      description="SMPP Library for Python",
+      author="Martynas Jocius",
+      author_email="mjoc@akl.lt",
+      url="http://mjoc.sig.lt/projects/smpplib/",
+      maintainer="Tim Akinbo",
+      maintainer_email="takinbo@python.org.ng",
+      license="LGPL",
+      keywords="smpp smpplib",
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Telephony',
+        'Topic :: System :: Networking',
+      ],
+      packages=['smpplib'],)
